3cd0b7285c33cac828d5dae8d7d96c859dd18569
Jason Stenhouse <jason.stenhouse@gmail.com>
2016-01-27 17:31:23 +0200

    initial commit

---
+++ src/main/java/com/stenhouse/cassandra/bitcoin/Application.java
@@ -0,0 +1,16 @@
+package com.stenhouse.cassandra.bitcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jason on 1/27/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
